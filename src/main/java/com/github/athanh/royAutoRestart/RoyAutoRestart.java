aa0adff9512c070d559a42ae76be124a5c26983d
thành cuốn <119925021+thanhcuon@users.noreply.github.com>
2025-04-23 12:06:32 +0700

    Update RoyAutoRestart.java

--- src/main/java/com/github/athanh/royAutoRestart/RoyAutoRestart.java
+++ src/main/java/com/github/athanh/royAutoRestart/RoyAutoRestart.java
@@ -1,4 +1,6 @@
 package com.github.athanh.royAutoRestart;
+
+import com.github.athanh.royAutoRestart.commands.RoyAutoRestartCommand;
 import com.github.athanh.royAutoRestart.config.RestartConfig;
 import com.github.athanh.royAutoRestart.manager.RestartManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -12,11 +14,30 @@ public final class RoyAutoRestart extends JavaPlugin {
         saveDefaultConfig();
         config = new RestartConfig(this);
         restartManager = new RestartManager(this, config);
+
+        // Register commands
+        RoyAutoRestartCommand mainCommand = new RoyAutoRestartCommand(this);
+        getCommand("royautorestart").setExecutor(mainCommand);
+        getCommand("royautorestart").setTabCompleter(mainCommand);
+
         getLogger().info("RoyAutoRestart has been enabled!");
     }
 
     @Override
     public void onDisable() {
         getLogger().info("RoyAutoRestart has been disabled!");
+    }
+
+    public void reloadConfiguration() {
+        reloadConfig();
+        config = new RestartConfig(this);
+
+        if (restartManager != null) {
+            restartManager.stop();
+        }
+        restartManager = new RestartManager(this, config);
+    }
+    public RestartManager getRestartManager() {
+        return restartManager;
     }
 }
